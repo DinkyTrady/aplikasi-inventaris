7312323deb9642ae5a34bef5f910e8c3f4b6d1bc
DinkyTrady <DinkyTrady@kyra.com>
2024-10-26 22:53:29 +0700

    rubah file berada

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
 # This file only can run on UNIX based system such as Linux, MacOS, and Android
-javac -d out src/Barang.java src/Main.java
+javac -d out app/src/Barang.java app/src/Main.java
 java -cp out src/Main
