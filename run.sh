c66e5e39d74f021493e80dc338bd552c1186b1bf
DinkyTrady <DinkyTrady@kyra.com>
2024-10-26 14:28:21 +0700

    [Fix]: explanation

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
-# This file only can run UNIX based system such as Linux or MacOS or Android
+# This file only can run on UNIX based system such as Linux, MacOS, and Android
 javac -d out src/Barang.java src/Main.java
 java -cp out src/Main
